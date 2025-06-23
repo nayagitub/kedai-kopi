034cbd13ea29160cd21c5863c692ef1a53343d7c
nayagitub <nayy8618@gmail.com>
2025-06-23 23:42:14 +0700

    Search Form

--- js/script.js
+++ js/script.js
@@ -6,6 +6,16 @@ document.querySelector("#hamburger-menu").onclick = () => {
   navbarNav.classList.toggle("active");
 };
 
+//Toggle class active untuk serach form
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
+
+document.querySelector("#search-button").onclick = (e) => {
+  searchForm.classList.toggle("active");
+  searchBox.focus();
+  e.preventDefault();
+};
+
 // klik diluar sidebar untuk menghilangkan sidebar
 const hamburger = document.querySelector("#hamburger-menu");
 
